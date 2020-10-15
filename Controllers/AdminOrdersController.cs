78c5cdebf8c21d611bba2c8318bf09508c0a2622
p4nd4ta <stu1901681077@uni-plovdiv.bg>
2020-10-15 22:42:09 +0300

    AdminOrder - Edit Removed

--- Controllers/AdminOrdersController.cs
+++ Controllers/AdminOrdersController.cs
@@ -91,58 +91,6 @@ namespace Drinks_Self_Learn.Controllers
             return View(order);
         }
 
-        // GET: OrdersAdmin/Edit/5
-        [Authorize(Roles = "Administrator")]
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var order = await _context.Orders.FindAsync(id);
-            if (order == null)
-            {
-                return NotFound();
-            }
-            return View(order);
-        }
-        // POST: OrdersAdmin/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [Authorize(Roles = "Administrator")]
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("OrderId,FirstName,LastName,AddressLine1,AddressLine2,ZipCode,State,Country,PhoneNumber,Email")] Order order)
-        {
-            if (id != order.OrderId)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(order);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!OrderExists(order.OrderId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(order);
-        }
-
         [Authorize(Roles = "Administrator")]
         // GET: OrdersAdmin/Delete/5
         public async Task<IActionResult> Delete(int? id)
