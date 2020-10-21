1d323403d8b04677028bb7d06a9b5dc572c72e26
p4nd4ta <stu1901681077@uni-plovdiv.bg>
2020-10-21 15:15:27 +0300

    BugFix: order id out of range

--- Controllers/AdminOrdersController.cs
+++ Controllers/AdminOrdersController.cs
@@ -36,9 +36,18 @@ namespace Drinks_Self_Learn.Controllers
         // GET: OrdersAdmin/Details/5
         public async Task<IActionResult> Details(int? id)
         {
-
             var order = await _context.Orders.Include(s => s.OrderLines)
                         .FirstOrDefaultAsync(m => m.OrderId == id);
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            if (order == null)
+            {
+                return NotFound();
+            }
 
             var drinksList = new List<Drink>();
             IEnumerable<OrderDetail> detailsList;
@@ -53,56 +62,37 @@ namespace Drinks_Self_Learn.Controllers
             ViewData["Drinks"] = drinksList;
             ViewData["Details"] = detailsList;
 
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            if (order == null)
-            {
-                return NotFound();
-            }
-
-            return View(order);
-        }
-
-        // GET: OrdersAdmin/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: OrdersAdmin/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("OrderId,FirstName,LastName,AddressLine1,AddressLine2,ZipCode,State,Country,PhoneNumber,Email")] Order order)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(order);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
             return View(order);
         }
 
         // GET: OrdersAdmin/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
+            var order = await _context.Orders.Include(s => s.OrderLines)
+                       .FirstOrDefaultAsync(m => m.OrderId == id);
+
             if (id == null)
             {
                 return NotFound();
             }
 
-            var order = await _context.Orders
-                .FirstOrDefaultAsync(m => m.OrderId == id);
             if (order == null)
             {
                 return NotFound();
             }
+
+            var drinksList = new List<Drink>();
+            IEnumerable<OrderDetail> detailsList;
+
+            foreach (OrderDetail od in order.OrderLines)
+            {
+                drinksList.Add(await _context.Drinks.FirstOrDefaultAsync(m => m.DrinkId == od.DrinkId));
+            }
+
+            detailsList = _context.OrderDetails.Where(p => p.OrderId.Equals(order.OrderId));
+
+            ViewData["Drinks"] = drinksList;
+            ViewData["Details"] = detailsList;
 
             return View(order);
         }
